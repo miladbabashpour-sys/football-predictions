61df2e56aea74ce1a83f7d523106eaa940c44213
miladbabashpour-sys <miladbabashpour@gmail.com>
2026-07-19 17:39:53 +0330

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,74 +1,7 @@
-async function register() {
-
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirmPassword").value;
-
-
-    if(password.length !== 4 || isNaN(password)) {
-        alert("رمز باید دقیقا ۴ رقم باشد");
-        return;
-    }
-
-
-    if(password !== confirmPassword) {
-        alert("رمزها یکی نیستند");
-        return;
-    }
-
-
-    const { data, error } = await supabaseClient
-    .from("profiles")
-    .insert([
-        {
-            username: username,
-            password: password,
-            points: 50
-        }
-    ]);
-
-
-    if(error) {
-        alert("ثبت نام انجام نشد");
-        console.log(error);
-    }
-    else {
-        alert("ثبت نام موفق بود. ۵۰ امتیاز هدیه گرفتی 🎁");
-    }
-
+function register() {
+    alert("دکمه ثبت نام کار می‌کند");
 }
 
-
-
-async function login() {
-
-    let username = document.getElementById("loginUsername").value;
-    let password = document.getElementById("loginPassword").value;
-
-
-    const { data, error } = await supabaseClient
-    .from("profiles")
-    .select("*")
-    .eq("username", username)
-    .eq("password", password)
-    .single();
-
-
-    if(error) {
-
-        alert("نام کاربری یا رمز اشتباه است");
-
-    }
-
-    else {
-
-        alert("خوش آمدی " + username);
-
-        localStorage.setItem(
-            "user",
-            JSON.stringify(data)
-        );
-
-    }
-
+function login() {
+    alert("دکمه ورود کار می‌کند");
 }
