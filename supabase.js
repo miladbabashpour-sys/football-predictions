03241bd526be4caf23d9b4ebe0b6e9ac352929f5
miladbabashpour-sys <miladbabashpour@gmail.com>
2026-07-19 17:15:33 +0330

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://ffauqosscnjlfsfmzfym.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_KTyGoh2PdQOp9I9iuEUH1w_wK42P32o";
 
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
